23587fa3d8e5a87fc048baa5deab81aeb493ccf2
Alex Petrov <petrov.alex.master@yandex.ru>
2024-03-28 21:31:26 +0300

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,22 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <TodoForm />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import TodoForm from "./components/TodoForm.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld,
+    TodoForm,
   },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "../node_modules/bootstrap/dist/css/bootstrap.css";
 </style>
---
+++ src/components/TodoForm.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="mb-4">
+    <h1>Vue 3</h1>
+    <h2>TypeScript Demo {{ version }}</h2>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+import { TodoType } from "@/models/todoModel";
+import { v4 as uuidv4 } from "uuid";
+
+export default defineComponent({
+  name: "TodoForm",
+  setup() {
+    // local state
+    const version = ref("v1");
+    const newTodo = ref("");
+
+    const todos = ref<TodoType[]>([]);
+
+    function addNewTodo(): void {
+      if (!newTodo.value) return;
+
+      todos.value.push({
+        id: uuidv4(),
+        done: false,
+        content: newTodo.value,
+      });
+
+      console.log("newTodo:", newTodo.value);
+
+      newTodo.value = "";
+    }
+
+    return { version };
+  },
+});
+</script>
